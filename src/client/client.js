aa7549ef9a65e26def84e0fda1c335e6d0d04b63
William del Rosario <williamdelrosario@yahoo.com>
2019-05-11 22:52:54 +0200

    Route configuration

--- src/client/client.js
+++ src/client/client.js
@@ -1,5 +1,11 @@
-import React    from 'react';
-import ReactDOM from 'react-dom';
-import Home     from './components/Home';
+import React             from 'react';
+import ReactDOM          from 'react-dom';
+import { BrowserRouter } from './components/Home';
+import Routes             from './Routes';
 
-ReactDOM.hydrate(<Home/>, document.querySelector('#root'));
+ReactDOM.hydrate(
+    <BrowserRouter>
+        <Routes />
+    </BrowserRouter>,
+    document.querySelector('#root')
+);

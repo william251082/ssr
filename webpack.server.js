872793e4d511d8b19fd783d5ad8f7388dac6b4c8
William del Rosario <williamdelrosario@yahoo.com>
2019-05-11 22:23:17 +0200

    Ignoring files with webpack

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.js');
+const webpackNodeExternals = require('webpack-node-externals');
 
 const config = {
     // Inform webpack that we're building a bundle for nodejs, rather than for the browser
@@ -14,6 +15,9 @@ const config = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'build')
     },
+
+    // Copies the entire source code of express, react, react-dom
+    externals: [webpackNodeExternals()]
 };
 
 module.exports = merge(baseConfig, config);

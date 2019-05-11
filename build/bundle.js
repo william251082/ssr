017744de6f1a560549d6fa5d31f66a7e89889c69
William del Rosario <williamdelrosario@yahoo.com>
2019-05-11 20:37:29 +0200

    Client Side js

--- build/bundle.js
+++ build/bundle.js
@@ -7034,14 +7034,26 @@ if (process.env.NODE_ENV === 'production') {
 "use strict";
 
 
-var express = __webpack_require__(53);
-var React = __webpack_require__(16);
-var renderToString = __webpack_require__(109).renderToString;
-var Home = __webpack_require__(120).default;
-var app = express();
+var _express = __webpack_require__(53);
+
+var _express2 = _interopRequireDefault(_express);
+
+var _react = __webpack_require__(16);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _server = __webpack_require__(109);
+
+var _Home = __webpack_require__(120);
+
+var _Home2 = _interopRequireDefault(_Home);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)();
 
 app.get('/', function (req, res) {
-    var content = renderToString(React.createElement(Home, null));
+    var content = (0, _server.renderToString)(_react2.default.createElement(_Home2.default, null));
 
     res.send(content);
 });
@@ -22453,7 +22465,18 @@ var Home = function Home() {
     return _react2.default.createElement(
         'div',
         null,
-        'Home Component!'
+        _react2.default.createElement(
+            'div',
+            null,
+            'Home Component'
+        ),
+        _react2.default.createElement(
+            'button',
+            { onClick: function onClick() {
+                    return console.log('Hi');
+                } },
+            'Press me'
+        )
     );
 };
 
--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Home = () => {
-    return <div>Home Component!</div>
+    return (
+        <div>
+            <div>
+                Home Component
+            </div>
+            <button onClick={ () => console.log('Hi') }>Press me</button>
+        </div>
+    )
 };
 
 export default Home;

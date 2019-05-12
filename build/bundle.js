2623c16a1ab9ab2978641bfa2fd756b1607bc460
William del Rosario <williamdelrosario@yahoo.com>
2019-05-12 21:32:02 +0200

    mitigating xss attack

--- build/bundle.js
+++ build/bundle.js
@@ -239,6 +239,10 @@ var _reactRedux = __webpack_require__(2);
 
 var _reactRouterConfig = __webpack_require__(18);
 
+var _serializeJavascript = __webpack_require__(21);
+
+var _serializeJavascript2 = _interopRequireDefault(_serializeJavascript);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.default = function (req, store) {
@@ -256,7 +260,7 @@ exports.default = function (req, store) {
         )
     ));
 
-    return '\n        <html>\n            <head></head>\n            <body>\n                <div id ="root">' + content + '</div>\n                <script >\n                    window.INITIAL_STATE = ' + JSON.stringify(store.getState()) + '\n                </script>\n                <script src="bundle.js"></script>\n            </body>\n        </html>\n    ';
+    return '\n        <html>\n            <head></head>\n            <body>\n                <div id ="root">' + content + '</div>\n                <script >\n                    window.INITIAL_STATE = ' + (0, _serializeJavascript2.default)(store.getState()) + '\n                </script>\n                <script src="bundle.js"></script>\n            </body>\n        </html>\n    ';
 };
 
 /***/ }),
@@ -524,6 +528,12 @@ exports.default = {
     loadData: loadData,
     component: (0, _reactRedux.connect)(mapStateToProps, { fetchUsers: _actions.fetchUsers })(UsersList)
 };
+
+/***/ }),
+/* 21 */
+/***/ (function(module, exports) {
+
+module.exports = require("serialize-javascript");
 
 /***/ })
 /******/ ]);
--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -3,7 +3,8 @@ import { renderToString } from 'react-dom/server';
 import { StaticRouter }   from 'react-router-dom';
 import Routes             from "../client/Routes";
 import { Provider }       from 'react-redux';
-import { renderRoutes }   from "react-router-config";
+import { renderRoutes }   from 'react-router-config';
+import serialize          from 'serialize-javascript';
 
 export default (req, store) => {
     const content = renderToString(
@@ -20,7 +21,7 @@ export default (req, store) => {
             <body>
                 <div id ="root">${ content }</div>
                 <script >
-                    window.INITIAL_STATE = ${JSON.stringify(store.getState())}
+                    window.INITIAL_STATE = ${ serialize(store.getState()) }
                 </script>
                 <script src="bundle.js"></script>
             </body>

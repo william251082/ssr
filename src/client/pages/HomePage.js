29ec050ffd75e2309e3560135272cf9da46acb3e
William del Rosario <williamdelrosario@yahoo.com>
2019-05-12 20:43:48 +0200

    refactored pages

--- src/client/pages/HomePage.js
+++ src/client/pages/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Home = () => {
+const HomePage = () => {
     return (
         <div>
             <div>
@@ -11,4 +11,4 @@ const Home = () => {
     )
 };
 
-export default Home;
+export default HomePage;

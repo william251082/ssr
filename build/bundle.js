3fc59efe6f82e432fed8950379b40ba70941096e
William del Rosario <williamdelrosario@yahoo.com>
2019-05-12 07:49:56 +0200

    Server store creation - serverside

--- build/bundle.js
+++ build/bundle.js
@@ -88,13 +88,21 @@ var _renderer = __webpack_require__(3);
 
 var _renderer2 = _interopRequireDefault(_renderer);
 
+var _createStore = __webpack_require__(8);
+
+var _createStore2 = _interopRequireDefault(_createStore);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
 
 app.use(_express2.default.static('public'));
 app.get('*', function (req, res) {
-    res.send((0, _renderer2.default)(req));
+    var store = (0, _createStore2.default)();
+
+    // Some logic to initialize and load data into the store
+
+    res.send((0, _renderer2.default)(req, store));
 });
 
 app.listen(3000, function () {
@@ -130,13 +138,19 @@ var _Routes = __webpack_require__(7);
 
 var _Routes2 = _interopRequireDefault(_Routes);
 
+var _reactRedux = __webpack_require__(11);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.default = function (req) {
+exports.default = function (req, store) {
     var content = (0, _server.renderToString)(_react2.default.createElement(
-        _reactRouterDom.StaticRouter,
-        { location: req.path, context: {} },
-        _react2.default.createElement(_Routes2.default, null)
+        _reactRedux.Provider,
+        { store: store },
+        _react2.default.createElement(
+            _reactRouterDom.StaticRouter,
+            { location: req.path, context: {} },
+            _react2.default.createElement(_Routes2.default, null)
+        )
     ));
 
     return '\n        <html>\n            <head></head>\n            <body>\n                <div id ="root">' + content + '</div>\n                <script src="bundle.js"></script>\n            </body>\n        </html>\n    ';
@@ -225,6 +239,49 @@ exports.default = function () {
             } })
     );
 };
+
+/***/ }),
+/* 8 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _redux = __webpack_require__(9);
+
+var _reduxThunk = __webpack_require__(10);
+
+var _reduxThunk2 = _interopRequireDefault(_reduxThunk);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = function () {
+    var store = (0, _redux.createStore)(reducers, {}, (0, _redux.applyMiddleware)(_reduxThunk2.default));
+
+    return store;
+};
+
+/***/ }),
+/* 9 */
+/***/ (function(module, exports) {
+
+module.exports = require("redux");
+
+/***/ }),
+/* 10 */
+/***/ (function(module, exports) {
+
+module.exports = require("redux-thunk");
+
+/***/ }),
+/* 11 */
+/***/ (function(module, exports) {
+
+module.exports = require("react-redux");
 
 /***/ })
 /******/ ]);
--- src/client/client.js
+++ src/client/client.js
@@ -3,7 +3,7 @@ import ReactDOM                        from 'react-dom';
 import { BrowserRouter }               from 'react-router-dom';
 import thunk                           from 'redux-thunk';
 import Routes                          from './Routes';
-import { Provider }                    from 'redux-thunk';
+import { Provider }                    from 'react-redux';
 import { createStore, applyMiddleware} from 'redux';
 
 const store = createStore(reducers, {}, applyMiddleware());

5153717acf33204ed8b712e0e4229fb1ae1945fa
William del Rosario <williamdelrosario@yahoo.com>
2019-05-12 07:35:34 +0200

    Browser store creation - clientside

--- src/client/client.js
+++ src/client/client.js
@@ -1,11 +1,18 @@
-import React             from 'react';
-import ReactDOM          from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import Routes            from './Routes';
+import React                           from 'react';
+import ReactDOM                        from 'react-dom';
+import { BrowserRouter }               from 'react-router-dom';
+import thunk                           from 'redux-thunk';
+import Routes                          from './Routes';
+import { Provider }                    from 'redux-thunk';
+import { createStore, applyMiddleware} from 'redux';
+
+const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.hydrate(
-    <BrowserRouter>
-        <Routes/>
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <Routes/>
+        </BrowserRouter>
+    </Provider>,
     document.querySelector('#root')
 );

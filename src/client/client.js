ddd92794b9c00b0ccc48b956f8a1e4efb4935b1f
William del Rosario <williamdelrosario@yahoo.com>
2019-05-12 08:06:35 +0200

    Reducer imports

--- src/client/client.js
+++ src/client/client.js
@@ -1,15 +1,16 @@
-import React                           from 'react';
-import ReactDOM                        from 'react-dom';
-import { BrowserRouter }               from 'react-router-dom';
-import thunk                           from 'redux-thunk';
-import Routes                          from './Routes';
-import { Provider }                    from 'react-redux';
-import { createStore, applyMiddleware} from 'redux';
+import React                            from 'react';
+import ReactDOM                         from 'react-dom';
+import { BrowserRouter }                from 'react-router-dom';
+import thunk                            from 'redux-thunk';
+import Routes                           from './Routes';
+import { Provider }                     from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers                         from './reducers';
 
 const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.hydrate(
-    <Provider store={store}>
+    <Provider store={ store }>
         <BrowserRouter>
             <Routes/>
         </BrowserRouter>

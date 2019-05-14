556dde03a6b218d08490b8b59224d66a461326ec
William del Rosario <williamdelrosario@yahoo.com>
2019-05-14 07:16:02 +0200

    Admins reducer and creator

--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import userReducer         from './usersReducer';
 import authReducer         from "./authReducer";
+import adminsReducer       from "./adminsReducer";
 
 export default combineReducers({
     users: userReducer,
-    auth: authReducer
+    auth: authReducer,
+    admins: adminsReducer
 });
